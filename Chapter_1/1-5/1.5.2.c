fd376332287a40bc684253e7b422987449c661d6
Jackie <guosj.fnst@cn.fujitsu.com>
2020-10-09 14:49:16 +0800

    Count characters in input; 2nd version

--- Chapter_1/1-5/1.5.2.c
+++ Chapter_1/1-5/1.5.2.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count characters in input; 2st version */
 int main()
 {
-    long nc;
+    double nc;
 
-    nc = 0;
-    while (getchar() != EOF)
-        ++nc;
-    printf("%ld\n", nc);
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;	/* null statement */
+    printf("%.0f\n", nc);
 
     return 0;
 }
